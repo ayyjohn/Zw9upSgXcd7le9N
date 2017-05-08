2f4787a598fcbfac1449a20fb16d9e5ffd78bf1a
Alec Johnson <alecjohnson55@gmail.com>
2017-05-08 16:23:23 -0700

    add create method that parses url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,8 +1,16 @@
+require 'open-uri'
+
 class UrlsController < ApplicationController
 
   def create
-    @url = Url.new(url_params)
-
+    url = url_params
+    document = Nokogiri::HTML(open(url))
+    headers = document.css('h1').map(&:text)
+    sub_headers = document.css('h2').map(&:text)
+    sub_sub_headers = document.css('h3').map(&:text)
+    content = headers + sub_headers + sub_sub_headers
+    content = content.join(' ')
+    @url = url.new(url, content)
     if @url.save
       render :index
     else
@@ -18,6 +26,6 @@ class UrlsController < ApplicationController
   private
 
   def url_params
-    params.require(:url).permit(:content)
+    params.require(:url).permit(:url)
   end
 end
