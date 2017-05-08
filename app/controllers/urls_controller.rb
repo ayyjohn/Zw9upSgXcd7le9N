e77512d025cfb7823357bddbe9f417f609d0d2d8
Alec Johnson <alecjohnson55@gmail.com>
2017-05-08 15:04:22 -0700

    add create and index actions for urls

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,23 @@
 class UrlsController < ApplicationController
+
+  def create
+    @url = Url.new(url_params)
+
+    if @url.save
+      render :index
+    else
+      render json: @url.errors.full_messages, status: 422
+    end
+  end
+
+  def index
+    @urls = Url.all
+    render :index
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:content)
+  end
 end
